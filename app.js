dced4c1a703b346d2042619ea3a9cea59e3b1ba3
AaronL360 <AAronl214@nycstudents.net>
2026-09-17 14:02:48 -0400

    cx

---
+++ app.js
@@ -0,0 +1,8 @@
+const age = 100
+if (age > 65) {
+    console.log("Senior");
+} else if (age >= 18) {
+    console.log("Adult");
+} else {
+    console.log("Child");
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+const age = 10
+if (age > 65) {
+    console.log("Sernior");
+} else if (age >= 18) {
+    console.log("Adult");
+} else {
+    console.log("Child");
+}
